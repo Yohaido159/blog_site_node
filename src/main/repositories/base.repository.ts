3c2c2a261d327927c6f754169e664efb883aa2e4
yohai <yohai@networx.com>
2023-01-25 00:21:32 +0200

    start point init - 3 - make it correct

--- src/main/repositories/base.repository.ts
+++ src/main/repositories/base.repository.ts
@@ -7,3 +7,42 @@ interface IBaseRepository<T> {
 }
 
 export { IBaseRepository };
+
+// import { IBaseRepository } from '@repositories/base.repository';
+import { model, Schema } from 'mongoose';
+
+class BaseRepository<T> implements IBaseRepository<T> {
+  private model: any;
+
+  constructor(schemaModel: any) {
+    this.model = schemaModel;
+  }
+
+  async findAll() {
+    const entities = await this.model.find();
+    return entities;
+  }
+
+  async findById(id: string) {
+    const entity = await this.model.findById;
+    return entity;
+  }
+
+  async create(entity: T) {
+    const newEntity = await this.model.create(entity);
+    return newEntity;
+  }
+
+  async update(id: string, entity: T) {
+    const updatedEntity = await this.model;
+    return updatedEntity;
+  }
+
+  async delete(id: string) {
+    const deletedEntity = await this.model;
+    return deletedEntity;
+  }
+}
+
+export default BaseRepository;
+  
--- src/main/repositories/user.repository.ts
+++ src/main/repositories/user.repository.ts
@@ -1,14 +1,11 @@
-
 import { IUser } from '@entities/user.entity';
-import { IBaseRepository } from '@repositories/base.repository';
+import BaseRepository, { IBaseRepository } from '@repositories/base.repository';
 import { model, Schema } from 'mongoose';
 
 export interface IUserRepository extends IBaseRepository<IUser> {
   create(entity: { email: string; password: string }): Promise<IUser>;
   findByEmail(email: string): Promise<IUser>;
 }
-
-
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
@@ -17,14 +14,16 @@ const UserSchema = new Schema({
 
 const UserModel = model<IUser>('User', UserSchema);
 
-class UserRepository implements IUserRepository {
+class UserRepository extends BaseRepository<IUser> implements IUserRepository {
   async create(entity: { email: string; password: string }) {
     const user = await UserModel.create(entity);
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await UserModel.findOne({ email });
     return user;
   }
 }
 
 export default UserRepository;
-
-
-
